1cee88cdfef7ee3ea411e8a22902a9b1d2d50353
PeaChack <dev.peachack@gmail.com>
2023-08-22 18:02:19 +0300

    Add base fields

--- clever-bank/src/main/java/by/peachack/model/Account.java
+++ clever-bank/src/main/java/by/peachack/model/Account.java
@@ -1,4 +1,12 @@
 package by.peachack.model;
 
+import java.time.LocalDateTime;
+
 public class Account {
+    private String id;
+    private String currency;
+    private Client client;
+    private Bank bank;
+    private LocalDateTime openingDateTime;
+
 }
